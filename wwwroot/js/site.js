1a3a09adc57ebff79c85eaa9951fc054714733d1
victorchiaka <chiakavictor205@gmail.com>
2023-07-19 00:30:00 +0100

    feat:-file: added javascript file, and made some code

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var body = document.querySelector("body");
+body.style.backgroundImage =
+    'url("/images/one-person-standing-determined-extreme-adventure-generated-by-ai.jpg")';
+body.style.transition = "2s ease-in-out";
+
+function changeBackground() {
+    const images = [
+        'url("images/awesome.jpg")',
+        'url("images/dew.jpg")',
+        'url("images/nature1.jpg")',
+        'url("images/nature2.jpg")',
+        'url("images/quintin.jpg")',
+        'url("images/pexels.jpg")',
+        'url("images/marek-piwnicki.jpg")',
+        'url("images/galactic-night.jpg")'
+    ];
+
+    {
+        const backgroundImage = images[Math.floor(Math.random() * images.length)];
+        body.style.background = backgroundImage;
+        body.style.backgroundAttachment = "fixed";
+        body.style.backgroundPosition = "center";
+        body.style.backgroundSize = "cover";
+    }
+}
+
+setInterval(changeBackground, 5000);
